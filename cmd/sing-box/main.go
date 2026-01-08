429556451990dc13dff4edbc7cad88c6d3446b55
AL <originalmail1@gmail.com>
2026-01-08 17:50:36 +1000

    disable other stuff for windows

--- cmd/sing-box/main.go
+++ cmd/sing-box/main.go
@@ -1,4 +1,4 @@
-//go:build !generate
+//go:build !generate && !windows
 
 package main
 
---
+++ cmd/sing-box/main_windows.go
@@ -0,0 +1,57 @@
+//go:build windows && !generate
+
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/sagernet/sing-box/cmd/sing-box/ipc"
+	"github.com/sagernet/sing-box/include"
+	"github.com/sagernet/sing-box/log"
+)
+
+func main() {
+	globalCtx = context.Background()
+
+	ctx := include.Context(globalCtx)
+
+	logFactory, err := log.New(log.Options{
+		Context: ctx,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logFactory.Close()
+
+	logger := logFactory.Logger()
+	logger.Info("starting sing-box with Named Pipe IPC")
+
+	pipeServer, err := ipc.NewPipeServer(ctx, logger)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	osSignals := make(chan os.Signal, 1)
+	signal.Notify(osSignals, os.Interrupt, syscall.SIGTERM)
+	defer signal.Stop(osSignals)
+
+	ctx, cancel := context.WithCancel(globalCtx)
+	go func() {
+		select {
+		case <-osSignals:
+			logger.Info("received shutdown signal")
+			cancel()
+			pipeServer.Stop()
+		case <-ctx.Done():
+		}
+	}()
+
+	err = pipeServer.Start()
+	cancel()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
